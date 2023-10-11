a6b2a4759ca8ca699b94b46ee5a64c9e928e2e92
The-Man-M <143039832+The-Man-M@users.noreply.github.com>
2023-10-11 17:33:14 -0600

    primera subida

---
+++ Estructor.cs
@@ -0,0 +1,28 @@
+﻿struct libro
+{
+    public string Nombre;
+    public string Autor;
+    public int AñoPublicado;
+
+    public void MostarInfo()
+    {
+        Console.WriteLine($"Nombre del libro: {this.Nombre}\nAutor del libro: {this.Autor}\nAño de publicación: {this.AñoPublicado}");
+    }
+}
+struct Estante
+{
+    List<libro> libros = new List<libro> ();
+    public Estante(List<libro> poesia)
+    {
+        libros.Add(poesia);
+    }
+    public void poesia()
+    {
+        foreach (var poesia in libros)
+        {
+            Console.WriteLine(poesia.Nombre);
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace dia_11_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int menu =1;
+            Bibliteca sanjuan = new Bibliteca();
+            
+            do
+            {
+                sanjuan.nom(menu);
+
+
+                Console.WriteLine("Biblioteca");
+                Console.WriteLine("1. Poemas\n2. Ciencia ficción\n3. Novelas clásicas\n4. Filosofía\n5. Drama\n6. Salir\n");
+
+                while (!int.TryParse(Console.ReadLine(), out menu) || (menu<1|| menu>6))
+                {
+
+                    Console.WriteLine("Ingrese valor valido\n");
+                    Console.WriteLine("1. Poemas\n2. Ciencia ficción\n3. Novelas clásicas\n4. Filosofía\n5. Drama\n6. Salir\n");
+
+
+                }
+
+
+            } while (menu !=6);   
+        }
+    }
+}
